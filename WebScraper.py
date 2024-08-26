905ef3ea9910a4b23f15544296de103b927b02df
Adhi <thisisadhi@gmail.com>
2024-08-26 12:18:15 -0400

    Updated Scoring for volume

--- WebScraper.py
+++ WebScraper.py
@@ -2,38 +2,42 @@ from urllib.request import urlopen
 import re
 
 class PlayerStats():
-    def __init__(self, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z):
+    def __init__(self, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, aa, bb, cc, dd):
         self.id = a
         self.name = b
         self.position = c
         self.age = d
         self.team = e
         self.games_played = f
-        self.per = g
-        self.ts_pct = h
-        self.fg3a_per_fga_pct = i
-        self.fta_per_fga_pct = j
-        self.orb_pct  = k
-        self.drb_pct = l
-        self.trb_pct = m
-        self.ast_pct = n
-        self.stl_pct = o
-        self.blk_pct = p
-        self.tov_pct = q
-        self.usg_pct = r
-        self.ows = s
-        self.dws = t
-        self.ws = u
-        self.ws_per_48 = v
-        self.obpm = w
-        self.dbpm = x
-        self.bpm =y
-        self.vorp = z
+        self.games_started = g
+        self.minutes = h
+        self.field_goals_made = i
+        self.field_goals_attempted = j
+        self.field_goals_percentage = k
+        self.three_pointers_made = l
+        self.three_pointers_attempted = m
+        self.three_point_percentage = n
+        self.two_pointers_made = o
+        self.two_pointers_attempted = p
+        self.two_point_percentage = q
+        self.effective_field_goal_percentage = r
+        self.free_throws_made = s
+        self.free_throws_attempted = t
+        self.free_throws_percentage = u
+        self.offensive_rebounds = v
+        self.defensive_rebounds = w
+        self.total_rebounds = x
+        self.assists =y
+        self.steals = z
+        self.blocks = aa
+        self.turnovers = bb
+        self.personal_fouls = cc
+        self.points = dd
         self.fg_add = 'N/A'
         self.ts_add = 'N/A'
         self.scoring = -999999999999999999999999999999999999999
 
-url = "https://www.basketball-reference.com/leagues/NBA_2024_advanced.html"
+url = "https://www.basketball-reference.com/leagues/NBA_2024_totals.html"
 page = urlopen(url)
 html_bytes = page.read()
 html = html_bytes.decode("utf-8")
@@ -42,7 +46,7 @@ players = []
 excluded = []
 for i in player_stats:
     stats = i.split("</td>")
-    if (len(stats) == 29):
+    if (len(stats) == 30):
         stat_list = []
         for j in stats:
             x = re.findall("data-stat=\"[a-zA-Z0-9_]+\"", j)
@@ -51,12 +55,13 @@ for i in player_stats:
                 c = z[1:]
                 if (c != '\n'):
                     stat_list.append(c)
-        if(len(stat_list) < 26):
+        if(len(stat_list) < 30):
             excluded = stat_list[0]
             continue
         players.append(PlayerStats(stat_list[0], stat_list[1], stat_list[2], stat_list[3], stat_list[4], stat_list[5], stat_list[6], stat_list[7], stat_list[8], 
                                    stat_list[9], stat_list[10], stat_list[11], stat_list[12], stat_list[13], stat_list[14], stat_list[15], 
-                                    stat_list[16], stat_list[17], stat_list[18], stat_list[19], stat_list[20], stat_list[21], stat_list[22], stat_list[23], stat_list[24], stat_list[25]))
+                                    stat_list[16], stat_list[17], stat_list[18], stat_list[19], stat_list[20], stat_list[21], stat_list[22], stat_list[23], stat_list[24], 
+                                    stat_list[25], stat_list[26], stat_list[27], stat_list[28], stat_list[29]))
           
 url = "https://www.basketball-reference.com/leagues/NBA_2024_adj_shooting.html"
 page = urlopen(url)
@@ -104,17 +109,6 @@ for c in countDict:
         for p in players:
             if (p.name == c and p.team != 'TOT'):
                 players.remove(p)
-for p in players:
-    if (p.ts_add == 'N/A' or p.fg_add == 'N/A'):
-        continue
-    curr_stat = (0.5 * float(p.ts_add)) + float(p.fg_add)
-    curr_stat /= int(p.games_played)
-    count+=1
-    total+= curr_stat
-offset = -1 * total / count
-count = 0
-total = 0
-
 def mergeSort(arr):
     if (len(arr) > 1):
         mid = int(len(arr) / 2)
@@ -140,10 +134,23 @@ def mergeSort(arr):
     else:
         return arr
 
+
 for p in players:
     if (p.ts_add == 'N/A' or p.fg_add == 'N/A'):
         continue
-    curr_stat = (0.5 * float(p.ts_add)) + float(p.fg_add)
+    curr_stat = float(p.fg_add)
+    curr_stat /= int(p.games_played)
+    count+=1
+    total+= curr_stat
+offset = -1 * total / count
+count = 0
+total = 0
+
+
+for p in players:
+    if (p.ts_add == 'N/A' or p.fg_add == 'N/A'):
+        continue
+    curr_stat = float(p.fg_add) + (.25 * (int(p.field_goals_attempted) + (.44 * int(p.free_throws_attempted))))
     curr_stat /= int(p.games_played)
     curr_stat += offset
     p.scoring = curr_stat
