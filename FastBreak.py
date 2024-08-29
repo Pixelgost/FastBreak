2b96948ef05109480d3c1707cd507939b067bf7c
Pixelgost <thisisadhi@gmail.com>
2024-08-28 21:11:14 -0400

    Added new charts, updated model

--- FastBreak.py
+++ FastBreak.py
@@ -5,11 +5,13 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import optuna
+import random
 from pathlib import Path
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 import sys
+import matplotlib.pyplot as plt
 
 class PlayerStats():
     #initalize variables
@@ -438,7 +440,11 @@ class statHandler():
         for p in self.players:
             p.normal_vorp = p.vorp - mean
             p.normal_vorp /= std
-        
+        for p in self.players:
+            if (p.vorp == -sys.maxsize - 1 or p.defensive_win_share_normalized == -sys.maxsize - 1 
+            or p.playmaking == -sys.maxsize - 1 or p.scoring == -sys.maxsize - 1 
+            or p.rebounding == -sys.maxsize - 1):
+                self.players.remove(p)
         #print results if necessary
         if(pr):
             print()
@@ -622,7 +628,7 @@ class Performer():
             output = model(torch.tensor(example_input[i]).unsqueeze(0))
             # Example target (for training purposes)
             key = i
-            if (key == 'CHA' and int(self.year) < 2014):
+            if (key == 'CHA' and int(self.year) <= 2014):
                 key = 'CHA2005'
             example_target = torch.full((1, 1), self.nba_team_wins[self.nba_team_dict[key]])  # Example target labels for classification
             # Compute loss
@@ -640,6 +646,88 @@ class Performer():
     
     #show performance of the model without training it further
     def performModelNoUpdate(self):
+        s = statHandler(self.year)
+        players = s.calculateTopPlayers(False)
+        # Instantiate the model
+        model = SimpleNN()
+
+        # Define loss function and optimizer
+        criterion = nn.MSELoss()  # For classification problems
+        optimizer = optim.Adam(model.parameters(), lr=0.01)
+
+        #load trained model
+        checkpoint = torch.load('model.pth')
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+
+        # Example input (batch size of 2 for demonstration)
+        # Each input tensor has the shape (batch_size, 7, 4)
+        
+        #set up example input
+        example_input = {}
+        for p in players:
+            if (p.team not in self.nba_team_arrs):
+                self.nba_team_arrs[p.team] = [np.array([p.scoring, p.playmaking, p.rebounding, p.defensive_win_share_normalized], dtype=np.float32)]
+            elif(len(self.nba_team_arrs[p.team]) < 8):
+                self.nba_team_arrs[p.team].append(np.array([p.scoring, p.playmaking, p.rebounding, p.defensive_win_share_normalized], dtype=np.float32))
+        # Forward pass
+        for a in self.nba_team_arrs:
+            if(len(self.nba_team_arrs[a]) == 8):
+                example_input[a] = self.nba_team_arrs[a]
+        total_loss = 0
+        for i in example_input:
+            output = model(torch.tensor(example_input[i]).unsqueeze(0))
+            # Example target (for training purposes)
+            key = i
+            if (key == 'CHA' and int(self.year) <= 2014):
+                key = 'CHA2005'
+            example_target = torch.full((1, 1), self.nba_team_wins[self.nba_team_dict[key]])  # Example target labels for classification
+            # Compute loss
+            loss = criterion(output, example_target)
+            total_loss += loss.item()
+        total_loss /= len(example_input)
+        return total_loss
+
+    def performModelForTeam(self, team):
+        s = statHandler(self.year)
+        players = s.calculateTopPlayers(True)
+        # Instantiate the model
+        model = SimpleNN()
+
+        # Define loss function and optimizer
+        criterion = nn.MSELoss()  # For classification problems
+        optimizer = optim.Adam(model.parameters(), lr=0.01)
+
+        #load trained model
+        checkpoint = torch.load('model.pth')
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+
+        # Example input (batch size of 2 for demonstration)
+        # Each input tensor has the shape (batch_size, 7, 4)
+        
+        #set up example input
+        example_input = {}
+        for p in players:
+            if (p.team not in self.nba_team_arrs):
+                self.nba_team_arrs[p.team] = [np.array([p.scoring, p.playmaking, p.rebounding, p.defensive_win_share_normalized], dtype=np.float32)]
+            elif(len(self.nba_team_arrs[p.team]) < 8):
+                self.nba_team_arrs[p.team].append(np.array([p.scoring, p.playmaking, p.rebounding, p.defensive_win_share_normalized], dtype=np.float32))
+        # Forward pass
+        for a in self.nba_team_arrs:
+            if(len(self.nba_team_arrs[a]) == 8 and a == team):
+                example_input[a] = self.nba_team_arrs[a]
+        print('RESULTS FOR ' + team + ': ', self.year, )
+        for i in example_input:
+            output = model(torch.tensor(example_input[i]).unsqueeze(0))
+            # Example target (for training purposes)
+            key = i
+            if (key == 'CHA' and int(self.year) <= 2014):
+                key = 'CHA2005'
+            print(self.nba_team_dict[i], output.detach().numpy()[0][0], self.nba_team_wins[self.nba_team_dict[key]])
+            example_target = torch.full((1, 1), self.nba_team_wins[self.nba_team_dict[key]])  # Example target labels for classification
+
+    def showCase(self):
         s = statHandler(self.year)
         players = s.calculateTopPlayers(True)
         # Instantiate the model
@@ -668,24 +756,73 @@ class Performer():
         for a in self.nba_team_arrs:
             if(len(self.nba_team_arrs[a]) == 8):
                 example_input[a] = self.nba_team_arrs[a]
+        total_loss = 0
         print('RESULTS: ', self.year)
         for i in example_input:
             output = model(torch.tensor(example_input[i]).unsqueeze(0))
             # Example target (for training purposes)
             key = i
-            if (key == 'CHA' and int(self.year) < 2014):
+            if (key == 'CHA' and int(self.year) <= 2014):
                 key = 'CHA2005'
             example_target = torch.full((1, 1), self.nba_team_wins[self.nba_team_dict[key]])  # Example target labels for classification
             # Compute loss
             print(self.nba_team_dict[i], output.detach().numpy()[0][0], self.nba_team_wins[self.nba_team_dict[key]])
             loss = criterion(output, example_target)
+            total_loss += loss.item()
             print(f'Loss: {loss.item()}')
-            print()
-    
+
+        #plot maps for stats
+        for i in range(0, len(players)):
+            plt.plot(i, players[i].normal_vorp, 'bo')
+        plt.title("Top Players of " + self.year)
+        plt.xlabel("Players")
+        plt.ylabel("VORP")    
+        plt.savefig("vorpChart"+ self.year+".png")
+        plt.clf()
+
+        players.sort(key=lambda x: x.scoring, reverse=True)
+        for i in range(0, len(players)):
+            plt.plot(i, players[i].scoring, 'bo')
+        plt.title("Top Scorers of " + self.year)
+        plt.xlabel("Players")
+        plt.ylabel("Scoring")    
+        plt.savefig("scoringChart"+ self.year+".png")
+        plt.clf()
+
+
+        players.sort(key=lambda x: x.playmaking, reverse=True)
+        for i in range(0, len(players)):
+            plt.plot(i, players[i].playmaking, 'bo')
+        plt.title("Top Playmakers of " + self.year)
+        plt.xlabel("Players")
+        plt.ylabel("Playmaking")    
+        plt.savefig("playmakingChart"+ self.year+".png")
+        plt.clf()
+
+        players.sort(key=lambda x: x.rebounding, reverse=True)
+        for i in range(0, len(players)):
+            plt.plot(i, players[i].rebounding, 'bo')
+        plt.title("Top Rebounders of " + self.year)
+        plt.xlabel("Players")
+        plt.ylabel("Rebounding")    
+        plt.savefig("reboundingChart"+ self.year+".png")
+        plt.clf()
+
+        players.sort(key=lambda x: x.defensive_win_share_normalized, reverse=True)
+        for i in range(0, len(players)):
+            plt.plot(i, players[i].defensive_win_share_normalized, 'bo')
+        plt.title("Top Defenders of " + self.year)
+        plt.xlabel("Players")
+        plt.ylabel("Defense")    
+        plt.savefig("defenseChart"+ self.year+".png")
+        plt.clf()
+
+        total_loss /= len(example_input)
+        return total_loss
     #train model for x epochs
     def trainForEpochs(epochs):
         for e in range(0, epochs):
-            for i in range(1980, 2024):
+            for i in range(2000, 2024):
                 if (i % 10 != 3 and i % 10 != 4):
                     p = Performer(str(i))
                     p.performModel()
@@ -693,9 +830,13 @@ class Performer():
                 val = int(5 * e / (epochs / 20))
                 if (val > 0):
                     print(str(val)+ '%...')
-Performer.trainForEpochs(60)
-p = Performer("2024")
-p.performModelNoUpdate()
-
+                    test_years = ["2024", "2023", "2014", "2013"]
+                    index = random.randint(0, len(test_years) - 1)
+                    p = Performer(test_years[index])
+                    print("average loss:", p.performModelNoUpdate())
+#Performer.trainForEpochs(20)
+for i in range(2010, 2024):
+    p = Performer(str(i))
+    p.showCase()
 
 
